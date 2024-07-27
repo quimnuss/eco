05326de24a013362e25f9828cf8d7410e1befcd8
pol <pol.monso@somenergia.coop>
2024-07-27 18:30:42 +0200

    wip: fdg

---
+++ ui/glv_fdg.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+var fdg_nodes : Array[FDGNode]
+
+func reset_species():
+    pass
+
+func modify_species(index : int, new_mutuality : Array, new_growth : float):
+    pass
+
+func add_species(species_name : String, new_mutuality : Array, new_growth : float):
+    pass
